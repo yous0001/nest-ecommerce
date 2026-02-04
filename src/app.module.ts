0e1d5eb7748e8e59a5aec54d158e56ea72cd61cc
Yousef emad mikhail <mad33833@gmail.com>
2026-02-04 11:12:05 +0200

    add config and jwt modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/ecommerce-project'),
+    MongooseModule.forRoot(process.env.MONGODB_URI || ''),
     UserModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1h' },
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
